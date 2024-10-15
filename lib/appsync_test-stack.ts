716f7333b260ce44967a5edc37ad701eebb2ac4f
titusefferian <kiritosato@gmail.com>
2024-10-15 21:43:08 +0900

    finalize

--- lib/appsync_test-stack.ts
+++ lib/appsync_test-stack.ts
@@ -1,67 +1,64 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as appsync from 'aws-cdk-lib/aws-appsync';
-import * as path from 'path';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as appsync from "aws-cdk-lib/aws-appsync";
+import * as path from "path";
 export class AppsyncTestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    const api = new appsync.GraphqlApi(this, 'MyGraphqlAPI', {
-      name: 'MyGraphqlAPI',
-      definition: appsync.Definition.fromFile(path.join(__dirname, 'schema.graphql')),
+    const api = new appsync.GraphqlApi(this, "MyGraphqlAPI", {
+      name: "MyGraphqlAPI",
+      definition: appsync.Definition.fromFile(
+        path.join(__dirname, "schema.graphql")
+      ),
       authorizationConfig: {
         defaultAuthorization: {
           authorizationType: appsync.AuthorizationType.API_KEY,
-        }
-      }
+          apiKeyConfig: {
+            expires: cdk.Expiration.after(cdk.Duration.days(365)),
+            name: "1 year api key",
+            description: "1 year api key",
+          },
+        },
+      },
     });
 
-    const noneDS = new appsync.NoneDataSource(this, 'NoneDataSource', {
+    const noneDS = new appsync.NoneDataSource(this, "NoneDataSource", {
       api,
-      name: 'NoneDataSource',
-      description: 'None Data Source',
+      name: "NoneDataSource",
+      description: "None Data Source",
     });
 
-    new appsync.Resolver(this, 'PipelineResolver', {
-      api,
-      typeName: 'Query',
+    const firstFunction = new appsync.AppsyncFunction(this, "FirstFunction", {
+      api: api,
       dataSource: noneDS,
-      fieldName: 'listDemos',
-      code: appsync.Code.fromInline(`
-        // The before step
-        export function request(...args) {
-          console.log(args);
-          return {}
-        }
-    
-        // The after step
-        export function response(ctx) {
-          return [
-            { id: 'demo1', version: '1.0' },
-            { id: 'demo2', version: '2.0' }
-          ];
-        }
-      `),
+      name: "FirstFunction",
+      description: "FirstFunction",
       runtime: appsync.FunctionRuntime.JS_1_0_0,
+      code: appsync.Code.fromAsset(
+        path.join(__dirname, "function", "first-function.js")
+      ),
     });
 
-    new appsync.Resolver(this, 'NameFieldResolver', {
-      api,
-      typeName: 'demo',
-      fieldName: 'name', 
-      dataSource: noneDS, 
-      code: appsync.Code.fromInline(`
-        // Resolver for 'name' field
-        export function request(ctx) {
-          // You can add any logic here to fetch or compute the name
-          return {};
-        }
-
-        export function response(ctx) {
-          // Returning a fixed name value for demonstration purposes
-          return 'hello world';
-        }
-      `),
+    const secondFunction = new appsync.AppsyncFunction(this, "SecondFunction", {
+      api: api,
+      dataSource: noneDS,
+      name: "SecondFunction",
+      description: "SecondFunction",
       runtime: appsync.FunctionRuntime.JS_1_0_0,
+      code: appsync.Code.fromAsset(
+        path.join(__dirname, "function", "second-function.js")
+      ),
+    });
+
+    new appsync.Resolver(this, "ListDemoPipeline", {
+      api,
+      typeName: "Query",
+      fieldName: "listDemos",
+      code: appsync.Code.fromAsset(
+        path.join(__dirname, "resolver", "listDemosResolver.js")
+      ),
+      runtime: appsync.FunctionRuntime.JS_1_0_0,
+      pipelineConfig: [firstFunction, secondFunction],
     });
   }
 }
